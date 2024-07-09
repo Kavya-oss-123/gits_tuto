a3e345b286d1aa4536e159da1751e757d8359293
Kavya <likhithakavya5@gmail.com>
2024-07-09 15:34:54 +0530

    this  is  atrail

---
+++ area.java
@@ -0,0 +1,7 @@
+public class area{
+    public static void main(String[] args) {
+        int side = 4;
+        int area = 4*area;
+        System.out.println("Printing square area,"+area);
+    }
+}
